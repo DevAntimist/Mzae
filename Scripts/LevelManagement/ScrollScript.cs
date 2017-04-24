55d3b0e7fa4d27b5a493fad6fde2922cf86ae65a
Tristan0214 <grimm9186@gmail.com>
2017-04-24 11:26:13 +0200

    bACKgROUND

---
+++ Scripts/LevelManagement/ScrollScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollScript : MonoBehaviour {
+    Bounds bounds;
+    Mesh mesh;
+   // Renderer rend;
+    //public float speed = 0;
+	// Use this for initialization
+	void Start () {
+        // rend = GetComponent<Renderer>();
+        mesh = GetComponent<MeshFilter>().mesh;
+       bounds = mesh.bounds;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+       
+	}
+    public void onCall()
+    {
+      //  Debug.Log("Double Crap: " + bounds.size.x);
+        transform.position = new Vector3(transform.position.x + (bounds.size.x * transform.localScale.x), transform.position.y, transform.position.z) ;
+    }
+}
