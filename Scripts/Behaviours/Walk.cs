1b36783f6666412dd65762320133ac74dc6ef9c0
Tristan0214 <grimm9186@gmail.com>
2017-04-24 08:10:51 +0200

    bACKgROUND

--- Scripts/Behaviours/Walk.cs
+++ Scripts/Behaviours/Walk.cs
@@ -7,6 +7,7 @@ public class Walk : AbstractBehaviour
     public float speed = 50f;
     public float runMultiplier = 2f;
     public bool running;
+    public GameObject Limiter;
 
     // Use this for initialization
     void Start ()
@@ -17,6 +18,7 @@ public class Walk : AbstractBehaviour
 	// Update is called once per frame
 	void Update ()
     {
+        var velX = 0f;
         running = false;
 
         var right = inputState.GetButtonValue(inputButtons[0]);
@@ -32,8 +34,13 @@ public class Walk : AbstractBehaviour
                 tmpSpeed *= runMultiplier;
                 running = true;
             }
+            velX = tmpSpeed * (float)inputState.direction;
 
-            var velX = tmpSpeed * (float)inputState.direction;
+            if (left)
+            {
+                if (transform.position.x < Limiter.transform.position.x)
+                    velX = 0;
+            }
             body2d.velocity = new Vector2(velX, body2d.velocity.y);
         }
     }
